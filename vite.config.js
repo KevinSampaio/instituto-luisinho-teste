a85e924a4c6724e6e64e69ee127298283e449bbe
KevinSampaio <kevin.sampaio@sptech.school>
2024-12-01 23:09:01 -0300

    fds fds fds

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'site',
   build: {
     outDir: 'dist', // Isso criará a pasta 'dist' dentro de 'site'
   },
